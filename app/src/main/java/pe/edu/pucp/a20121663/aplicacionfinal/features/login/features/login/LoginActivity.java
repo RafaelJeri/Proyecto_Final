106ccaefc22c2f8c63d8994c8831a214762e3fe9
Benjamin <b.castillo@pucp.pe>
2019-07-01 15:29:30 -0500

    Alpha 1.2

--- app/src/main/java/pe/edu/pucp/a20121663/aplicacionfinal/features/login/features/login/LoginActivity.java
+++ app/src/main/java/pe/edu/pucp/a20121663/aplicacionfinal/features/login/features/login/LoginActivity.java
@@ -1,4 +1,4 @@
-package pe.edu.pucp.a20121663.aplicacionfinal.features.login;
+package pe.edu.pucp.a20121663.aplicacionfinal.features.login.features.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
