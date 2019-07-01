b75a930deb73659ada211578f9f9a8b14d169e53
Benjamin <b.castillo@pucp.pe>
2019-07-01 16:08:38 -0500

    Alpha 1.8

---
+++ app/src/main/java/pe/edu/pucp/a20121663/aplicacionfinal/features/login/features/base/IPresenter.java
@@ -0,0 +1,5 @@
+package pe.edu.pucp.a20121663.aplicacionfinal.features.login.features.base;
+
+public interface IPresenter {
+    void onDestroy();
+}
---
+++ app/src/main/java/pe/edu/pucp/a20121663/aplicacionfinal/features/login/features/base/IView.java
@@ -0,0 +1,8 @@
+package pe.edu.pucp.a20121663.aplicacionfinal.features.login.features.base;
+
+import android.content.Context;
+
+public interface IView {
+    Context getContext();
+    void onDestroy();
+}
