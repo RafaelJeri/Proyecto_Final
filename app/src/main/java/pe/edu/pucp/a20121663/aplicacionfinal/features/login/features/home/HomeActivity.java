61807f8d2f0f9a98d951ed69d543b70df89e0a87
Benjamin <b.castillo@pucp.pe>
2019-07-01 18:54:13 -0500

    Beta 1.5

--- app/src/main/java/pe/edu/pucp/a20121663/aplicacionfinal/features/login/features/home/HomeActivity.java
+++ app/src/main/java/pe/edu/pucp/a20121663/aplicacionfinal/features/login/features/home/HomeActivity.java
@@ -6,25 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
 
 import pe.edu.pucp.a20121663.aplicacionfinal.R;
 import pe.edu.pucp.a20121663.aplicacionfinal.features.login.features.login.LoginActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
-        private TextView mFullName;
-        private TextView mEmail;
         private Button mLogout;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_home);
-            mFullName = findViewById(R.id.home_txt_fullname);
-            mEmail = findViewById(R.id.home_txt_email);
             mLogout = findViewById(R.id.home_btn_logout);
-            showUserDetails();
             initButtons();
         }
 
@@ -37,18 +32,6 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
-
-    private void showUserDetails() {
-            Bundle extras = getIntent().getExtras();
-            if (extras != null) {
-                // Obtener los valores enviados por el Intent
-                String fullName = extras.getString(LoginActivity.LOGIN_EXTRA_FULLNAME);
-                String email = extras.getString(LoginActivity.LOGIN_EXTRA_EMAIL);
-                // Colocar los valores en el TextView
-                mFullName.setText(fullName);
-                mEmail.setText(email);
-            }
-        }
         public void logout(View v) {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
