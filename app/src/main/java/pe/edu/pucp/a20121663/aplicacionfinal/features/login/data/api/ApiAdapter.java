459952518585cbfccc4aa19dc2f64522537e4e5d
alulab14 <renzo.ramirez@pucp.pe>
2019-07-01 16:46:04 -0500

    alpha 1.5

--- app/src/main/java/pe/edu/pucp/a20121663/aplicacionfinal/features/login/data/api/ApiAdapter.java
+++ app/src/main/java/pe/edu/pucp/a20121663/aplicacionfinal/features/login/data/api/ApiAdapter.java
@@ -11,6 +11,7 @@ public class ApiAdapter {
     public static final String APPLICATION_NAME = " MFL_MOBILE";
 
     private static final String BASE_URL = "http://demo7908276.mockable.io/my-first-login-rest/remote/";
+    //https://demo1928384.mockable.io/dispositivos-moviles-rest/proyecto
 
     private static final int TIMEOUT = 90; // tiempo en segundos
     private static ApiService INSTANCE;
